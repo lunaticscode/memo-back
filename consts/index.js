9ef748ae60358a52d9ee118d0c81efc58e44ef87
lunaticscode <lunatics384@gmail.com>
2024-01-01 17:12:27 +0900

    ColorLabel 변경

--- consts/index.js
+++ consts/index.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const APP_TOKEN = process.env.APP_TOKEN || "memo-app-token";
 
 const CALENDAR_LABELL_COLOR = {
-  GREEN: "#32F33A",
-  JAMONG: "#FF7700",
-  YELLOW: "#FFD700",
+  GREEN: "#67DC13",
+  JAMONG: "#FF7600D4",
+  YELLOW: "#ECE08A",
   BLACK: "#0c0c0c",
   RED: "#FF4A42",
-  PURPLE: "#DE4EFF",
-  SKYBLUE: "#4FE6FF",
+  PURPLE: "#BA76E4",
+  SKYBLUE: "#84A7E1",
 };
 
 Object.freeze(CALENDAR_LABELL_COLOR);
